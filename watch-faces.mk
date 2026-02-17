cf3446dee6a141ce052435bbeac8400050f36d52
dlorp <54248315+dlorp@users.noreply.github.com>
2026-02-16 19:48:41 -0900

    fix: add smart_alarm_face to build

--- watch-faces.mk
+++ watch-faces.mk
@@ -56,4 +56,5 @@ SRCS += \
   ./watch-faces/complication/lander_face.c \
   ./watch-faces/complication/simon_face.c \
   ./watch-faces/complication/ping_face.c \
+  ./watch-faces/complication/smart_alarm_face.c \
 # New watch faces go above this line.
